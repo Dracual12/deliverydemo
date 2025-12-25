8cecdcd721ab39f80efa9d53c9df1217393fde15
Dracual12 <73178504+Dracual12@users.noreply.github.com>
2025-12-25 06:37:53 +0300

    j

---
+++ naim/order_and_web_app.py
@@ -0,0 +1,5 @@
+from config import dp, bot, db
+from aiogram import types
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+
